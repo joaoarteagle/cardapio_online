5332a85e8499c64e36c8de12c0b688f83c3586ec
JoaoAndrade <joaopaulo.arteagle17@gmail.com>
2025-01-05 16:52:25 -0300

    alterando script do workflow add memoria no build, a caminho do deploy

--- src/components/items/whatsapp.icon.js
+++ src/components/items/whatsapp.icon.js
@@ -4,7 +4,7 @@ function WhatsappIcon(){
     return(
        
         <a href="https://wa.me/5544998006615?text=Ol%C3%A1%20Lanchonete%20Caiu%C3%A1!"  target="_blank" rel="noreferrer">
-            <img src={`${process.env.PUBLIC_URL}/images/whatsapp.png`} className={style.whatsapp} alt=''whatsappIcon/>
+            <img src={`${process.env.PUBLIC_URL}/images/whatsapp.png`} className={style.whatsapp} alt='whatsappIcon'/>
         </a>
        
     );
